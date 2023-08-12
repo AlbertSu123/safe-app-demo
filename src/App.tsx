8c9bbaf77f78c3dc42b1851d6e918d84eaf32431
Albert Su <albertsu2001@gmail.com>
2023-08-12 03:02:22 -0700

    ✨able to get owners

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
 import { useSafeBalances } from './hooks/useSafeBalances';
 import BalancesTable from './components/BalancesTable';
 import { getTransferTransaction } from "./apis/transfers";
+import { ethers } from 'ethers';
+// import { FunWallet } from "@fun-xyz/core";
+// import { keccak256, toBytes } from "viem";
 
 const Container = styled.div`
   padding: 1rem;
@@ -22,12 +25,43 @@ const SafeApp = (): React.ReactElement => {
   const [recipient, setRecipient] = React.useState('');
 
   const handleTransfer = async (): Promise<void> => {
+    const safeAddress = safe.safeAddress
+    const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/ev5UT9SghDasYMQqBwAgKCm0axotZVAi")
+    const safeContract: ethers.Contract = new ethers.Contract(safeAddress, [{
+      "inputs": [],
+      "name": "getOwners",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }], provider)
+    const owners = await safeContract.getOwners()
+    console.log(owners)
+    // const wallet = new FunWallet({
+    //   users: [{ userId: owners[0] }],
+    //   uniqueId: keccak256(toBytes(Math.ceil(Math.random() * 100000)))
+    // })
+    // console.log(wallet)
     const transactions = balances.map((balance) => getTransferTransaction(balance, recipient));
 
     const { safeTxHash } = await sdk.txs.send({ txs: transactions });
 
     console.log({ safeTxHash });
   };
+
+  // Get the owners of the gnosis safe
+  // Create the funwallet with multiple users
+  // const wallet = new FunWallet({
+  //   users: [{ userId: await auth.getAddress() }],
+  //   uniqueId: keccak256(toBytes(Math.ceil(Math.random() * 100000)))
+  // })
+  // Initialize the funwallet
+  // Return the address of the funwallet
 
   return (
     <Container>
